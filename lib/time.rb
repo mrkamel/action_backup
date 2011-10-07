2c018fa4fa15c0a104bcf53fdc3fd03a8e9d30f7
Benjamin Vetter <vetter@flakks.com>
2011-10-07 10:38:17 +0200

    time extended

--- lib/time.rb
+++ lib/time.rb
@@ -3,5 +3,17 @@ class Time
   [ :sunday?, :monday?, :tuesday?, :wednesday?, :thursday?, :friday?, :saturday? ].each_with_index do |method, index|
     define_method(method) { wday == index }
   end
+
+  def first_day_of_month?
+    return mday == 1
+  end
+
+  def first_week_of_month?
+    return mday.between?(1, 7)
+  end 
+
+  def first_month_of_quarter?
+    return (Time.now.month - 1) % 3 == 0
+  end 
 end
 
