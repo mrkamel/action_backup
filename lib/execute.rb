e575485ff2a7f38a97a5b8f355c7ef40c8100687
Benjamin Vetter <vetter@plainpicture.de>
2013-07-09 09:52:56 +0200

    Added a :secret option to execute

--- lib/execute.rb
+++ lib/execute.rb
@@ -1,6 +1,13 @@
 
-def execute(cmd)
-  puts cmd
+def execute(cmd, options = {}) 
+  output = cmd.dup
+
+  Array(options[:secret]).each do |secret|
+    output.gsub! secret, "*****"
+  end 
+
+  puts output
+
   puts %x{ #{cmd} 2>&1 }
   
   $?.success?
